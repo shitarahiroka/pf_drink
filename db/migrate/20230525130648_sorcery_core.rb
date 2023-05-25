2209b8bc8504aa134deb6456623d30af8afec9ca
shitarahiroka <rem.moon4@gmail.com>
2023-05-25 22:15:46 +0900

    Userモデル作成

---
+++ db/migrate/20230525130648_sorcery_core.rb
@@ -0,0 +1,16 @@
+class SorceryCore < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email,            null: false, index: { unique: true }
+      t.string :crypted_password
+      t.string :salt
+      t.string :name, null: false
+      t.integer :weight
+      t.integer :age
+      t.integer :gender
+      t.integer :area
+
+      t.timestamps                null: false
+    end
+  end
+end
