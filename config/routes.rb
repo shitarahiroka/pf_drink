3aad879123d32ef76e3ca0eb819aaae81b543d9c
shitarahiroka <rem.moon4@gmail.com>
2023-05-29 14:40:13 +0900

    カフェイン制限量の設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  get 'profile/:id', to: 'profiles#show', as: 'profile'
+  get 'profile/:id/edit', to: 'profiles#edit', as: 'edit_profile'
+  patch 'profile/:id', to: 'profiles#update'
 
   resources :users, only: %i[new create]
   resources :profile, only: %i[show edit update]
