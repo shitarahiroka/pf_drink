1675d960335c57bceec5bb79af5c8fb1a6454133
shitarahiroka <rem.moon4@gmail.com>
2023-05-30 14:47:37 +0900

    ドリンクマイグレーションファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_25_130648) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_032150) do
+  create_table "drinks", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.float "caffeine"
+    t.integer "teanine"
+    t.integer "sweetness"
+    t.string "mood"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
