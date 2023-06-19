e692c013f531f4cddc4b45355670128612b78999
shitarahiroka <rem.moon4@gmail.com>
2023-06-19 17:15:42 +0900

    パスワード再設定の機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   get 'drinks/suggestion', to: 'drinks#suggestion', as: 'drinks_suggestion'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'login', to: 'user_sessions#new'
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
     end
   end
   resources :drink_records, only: %i[create show]
+  resources :password_resets, only: %i[new create edit update]
   # Defines the root path route ("/")
   root "tops#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_134412) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_060923) do
   create_table "drink_records", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "morning_suggestion"
@@ -47,7 +47,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_134412) do
     t.integer "area"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "drink_records", "drinks", column: "afternoon_suggestion"
