706a617d73ed4bf5a223dfd1d041d9a42def1467
shitarahiroka <rem.moon4@gmail.com>
2023-07-27 18:00:42 +0900

    マイグレーションファイル修正

--- db/migrate/20230612074305_change_data_types_in_drink_record.rb
+++ db/migrate/20230612074305_change_data_types_in_drink_record.rb
@@ -1,8 +1,8 @@
 class ChangeDataTypesInDrinkRecord < ActiveRecord::Migration[7.0]
   def up
-    change_column :drink_records, :morning_suggestion, :bigint
-    change_column :drink_records, :afternoon_suggestion, :bigint
-    change_column :drink_records, :evening_suggestion, :bigint
+    change_column :drink_records, :morning_suggestion, :bigint, using: 'morning_suggestion::bigint'
+    change_column :drink_records, :afternoon_suggestion, :bigint, using: 'afternoon_suggestion::bigint'
+    change_column :drink_records, :evening_suggestion, :bigint, using: 'evening_suggestion::bigint'
   end
 
   def down
